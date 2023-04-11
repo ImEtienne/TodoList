43428a3a32b40603675a0865a527f04408434313
Etienne KIBONGANI LOMBO <graddykibongani@gmail.com>
2023-04-11 21:50:34 +0200

    :bulb: added modal dialog and icon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
     <!-- CSS only -->
     <link rel="icon" href="icons-tasklist.png" type="image/x-icon"/>
     <link type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{asset('style.css')}}" rel="stylesheet" media="screen" type="text/css"/>
 </head>
 <body>
@@ -36,8 +37,6 @@
 
     </div>
 </div>
-
-
 
 @include('footer')
 <!-------------------------- JavaScript Bundle with Popper ------------------>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -26,9 +26,33 @@
                     <td>{{$todo -> nom}}</td>
                     <td>{{$todo -> created_at}}</td>
                     <td>{{$todo -> updated_at}}</td>
-                    <td class="mod"><a type="button" class="btn btn-primary" href="{{route('modform',['id'=>$todo->id])}}">Modifier</a></td>
-                    <td class="supp"><a type="button" class="btn btn-danger" href="{{route('delete',['id'=>$todo->id])}}">Supprimer</a></td>
+                    <td class="mod">
+                        <a type="button" class="btn btn-primary" href="{{route('modform',['id'=>$todo->id])}}"><i class="fa-regular fa-pen-to-square"></i>&nbsp;Modifier</a>
+                        <a type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal" ><i class="fa-solid fa-trash-can"></i>&nbsp;Supprimer</a>
+
+                        <!-- Modal Delete-->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        êtes-vous sûr de vouloir supprimer ?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                        <a type="button" class="btn btn-danger" href="{{route('delete',['id'=>$todo->id])}}"><i class="fa-solid fa-trash-can"></i>&nbsp;Supprimer</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </td>
                 </tr>
+
+                {{--href="{{route('delete',['id'=>$todo->id])}}"--}}
             @empty
                 <p style="color:red; font-weight: bold"> Aucune note ! </p>
             @endforelse
